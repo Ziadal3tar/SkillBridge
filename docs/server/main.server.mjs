2e7304f1f871ae5af266bd69c31a9403ca5cf213
ziadAlattar <ziadalattar22@gmail.com>
2024-09-12 20:49:30 +0300

    '12/9-1'

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7ZLJRENW.mjs";import"./chunk-DX76VUH7.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-K6RKUX6W.mjs";import"./chunk-DX76VUH7.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/components/display-instructors/display-instructors.component.ts
+++ src/app/components/display-instructors/display-instructors.component.ts
@@ -12,22 +12,22 @@ import { CommonModule } from '@angular/common';
 export class DisplayInstructorsComponent {
   Instructors:any[]=[
     {
-      imgSrc:'../../../assets/instractor1.jpg',
+      imgSrc:'./assets/instractor1.jpg',
       name:'Yousuf White',
       jobTitle:'UI/UX Exparet',
     },
     {
-      imgSrc:'../../../assets/instractor2.jpg',
+      imgSrc:'./assets/instractor2.jpg',
       name:'Elizabeth Olsen',
       jobTitle:'Assistant Teacher',
     },
     {
-      imgSrc:'../../../assets/instractor3.jpg',
+      imgSrc:'./assets/instractor3.jpg',
       name:'Jons Emon',
       jobTitle:'Assistant Teacher',
     },
     {
-      imgSrc:'../../../assets/instractor4.jpg',
+      imgSrc:'./assets/instractor4.jpg',
       name:'Mahmud Sujons',
       jobTitle:'Instructor',
     },
